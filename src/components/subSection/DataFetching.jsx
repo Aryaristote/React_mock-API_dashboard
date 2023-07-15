f3a7761be0b9f8c5767f5b3397429bcbd568534a
aristote <aryaristote@gmail.com>
2023-07-15 16:40:02 +0200

    another change

--- src/components/subSection/DataFetching.jsx
+++ src/components/subSection/DataFetching.jsx
@@ -1,17 +1,21 @@
-import React, {useEffect, useReducer} from 'react'
-import axios from 'axios';
-
-const initialState = {
-  loading: true,
-  error: '',
-  post: {},
-}
+import React, { useEffect, useState } from 'react'
 
 function DataFetching() {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(prev => prev + 1)
+    }, 2000)
+
+    return () => {
+      clearInterval(interval);
+    }
+  })
 
   return (
     <div>
-      
+      <h1>{timer}</h1>
     </div>
   )
 }
